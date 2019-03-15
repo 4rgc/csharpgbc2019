4d53cea83063c6c15875a49b95e2c087ccec53de
4rgc <justkrut514@outlook.com>
2019-03-15 12:36:58 -0400

    Added project files

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Rafed Fareed 101205956
+//Tuan Hai Phung 101170570
+//Andrii Bohdan 101171640
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = File.Open("testdata.txt", FileMode.Open);
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                string ans = sr.ReadLine();
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
