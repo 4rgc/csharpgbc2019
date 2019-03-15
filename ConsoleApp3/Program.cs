1e6ba3cdef1b30a6c300d85c387f98162b13adbc
Tuan Hai Phung <101170570@CLC330-015m.local>
2019-03-15 14:23:07 -0400

    Meet the requirements

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -13,14 +13,59 @@ namespace ConsoleApp3
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            FileStream fs = File.Open("testdata.txt", FileMode.Open);
-            using (StreamReader sr = new StreamReader(fs))
-            {
-                string ans = sr.ReadLine();
-                Console.WriteLine(ans);
-            }
-        }
+		static void Main(string[] args)
+		{
+			
+			FileStream fs = File.Open("testdata.txt", FileMode.Open);
+			using (StreamReader sr = new StreamReader(fs))
+			{
+				
+				string ans = sr.ReadLine();
+
+			    Console.WriteLine(ans + '\n');
+
+				string read = sr.ReadLine();
+				int students = 0;
+				int[] rightAnswers = new int[20];
+				while (read != "0")
+				{
+					students++;
+					string[] splitted = read.Split(' ');
+					read = sr.ReadLine();
+					int count = 0;
+					for (int x = 0; x < ans.Length; x++)
+					{
+						if (splitted[1][x] == 'X')
+						{
+							continue;
+						}
+						if (splitted[1][x] == ans[x])
+						{
+							rightAnswers[x] +=1 ;
+							count += 4;
+						}
+						else
+						{
+							count -= 1;
+						}
+					}
+				    Console.WriteLine($"Student number: {splitted[0]} got {count} points");
+				}
+                            
+				Console.WriteLine($"Total number of students: {students}");
+
+				Console.Write("Questions:\t");
+				for (int i = 1; i <= rightAnswers.Length; i++) {
+					Console.Write(i + "\t");
+				}
+				Console.WriteLine();
+				Console.Write("Answers: \t");
+				for (int i = 0; i < rightAnswers.Length; i++)
+                {
+                    Console.Write(rightAnswers[i] + "\t");
+                }
+			}         
+		}
+        
     }
 }
